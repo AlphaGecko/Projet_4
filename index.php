9a432817e09b525bd86de1f00bdcc6bbc18c6c79
AlphaGecko <mansour.jean-loup@protonmail.com>
2019-11-12 13:14:48 +0100

    fusion des deux index en conflit

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php session_start(); 
 
-<<<<<<< HEAD
-<!-- ROUTEUR -->
-
-<?php
-=======
 // ROUTEUR
 
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
 require_once('controllers/frontend/UserController.php'); 
 require_once('controllers/backend/AdminController.php'); 
 
@@ -15,52 +9,8 @@ $userView = new UserController();
 $adminView = new AdminController();
 
 if (isset($_GET['action'])) {
-<<<<<<< HEAD
     try 
     {
-<<<<<<< HEAD
-        $userView->billsList();
-    }
-
-    elseif ($_GET['action'] === 'bill') 
-    {
-        if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
-        { 
-            $userView->bills();
-        }
-
-        else 
-        {
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-        }
-    }
-
-
-    elseif ($_GET['action'] === 'addComment') 
-    { 
-
-        if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
-=======
-
-    try
-    {
-        if ($_GET['action'] === 'BillsList' || $_GET['action'] === 'allBills') 
-        {
-            $userView->billsList();
-        }
-    
-        elseif ($_GET['action'] === 'bill' && !isset($_GET['comment'])) 
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
-        {
-            if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
-            { 
-                $userView->bills();
-            }
-            else 
-            {
-<<<<<<< HEAD
-                $userView->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-=======
         if ($_GET['action'] === 'BillsList' || $_GET['action'] === 'allBills') 
         {
             $userView->billsList();
@@ -71,27 +21,6 @@ if (isset($_GET['action'])) {
             if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
             { 
                 $userView->bills();
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
-                $userView->error();
-            }
-        }
-    
-        elseif ($_GET['action'] === 'bill' && $_GET['comment'] === 'addComment') 
-        { 
-            if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
-            {
-    
-                if (!empty($_POST['author']) && !empty($_POST['comment'])) 
-                {
-                    $userView->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                    $userView->validation();
-                }
-                else 
-                {
-                    $userView->error();
-                }
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
             }
 
             else 
@@ -99,11 +28,6 @@ if (isset($_GET['action'])) {
                 $userView->error();
             }
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-        else 
-=======
     
     
         elseif ($_GET['action'] === 'bill' && $_GET['comment'] === 'addComment') 
@@ -128,7 +52,6 @@ if (isset($_GET['action'])) {
         }
     
         elseif($_GET['action'] === 'admin' && !isset($_GET['validation'])) 
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
         {
             $adminView->adminLogin();
         }
@@ -143,78 +66,13 @@ if (isset($_GET['action'])) {
             if (htmlspecialchars($_POST['admin_id']) === $adminName && htmlspecialchars($_POST['admin_password']) === $adminPassword)
             {
                 $_SESSION['admin'] = $data['admin_pseudo'];
-<<<<<<< HEAD
-                header('Location:https://www.alpha-gecko.com/Projet_4/'); 
-=======
                 $adminView->adminPanel();
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
-    
-        elseif($_GET['action'] === 'admin') 
-        {
-            $adminView->LogView();
-        }
-    
-        elseif($_GET['action'] === 'connexion') 
-        {
-            $adminView->adminDatas();
-    
-            if (htmlspecialchars($_POST['admin_name']) === $adminView->getAdminName() && htmlspecialchars($_POST['admin_password']) === $adminView->getAdminPassword())
-            {
-                $_SESSION['admin'] = $adminView->getAdminNickName();
-                $adminView->adminPanel();
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
             } 
             else 
             {
                 $userView->error();
             }
-<<<<<<< HEAD
         }
-<<<<<<< HEAD
-    }
-
-    elseif($_GET['action'] === 'newBillValidation')
-    {
-
-        $author = $_SESSION['admin']; 
-        
-        if (isset($_POST['title']) && isset($_POST['content']))
-        {
-            $adminView->validation();
-            $sendToDb = new AdminController; 
-            $sendToDb->addNewBill($_POST['title'], $_POST['content'], $author);
-=======
-            
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
-        }
-    
-        elseif($_GET['action'] === 'newBillValidation')
-        {
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
-    
-            $author = $_SESSION['admin']; 
-            
-            if (isset($_POST['title']) && isset($_POST['content']))
-            {
-                $adminView->validation();
-                $sendToDb = new AdminController; 
-                $sendToDb->addNewBill($_POST['title'], $_POST['content'], $author);
-            }
-            else 
-            {
-                throw new Exception('Vous devez impérativement poster un titre et un contenu. Faites "précédent" pour ne pas perdre votre travail.');
-            }
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
-        }
-    
-        elseif($_GET['action'] === 'deconnexion')
-        {
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
     
         elseif($_GET['action'] === 'newBillValidation')
         {
@@ -231,33 +89,10 @@ if (isset($_GET['action'])) {
             {
                 throw new Exception('Vous devez impérativement poster un titre et un contenu. Faites "précédent" pour ne pas perdre votre travail.');
             }
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
         }
     
         elseif($_GET['action'] === 'deconnexion')
         {
-            $adminView->deconnexion();
-        }
-    
-        elseif ($_GET['action'] === 'editBill')
-        {
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
-            if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
-            { 
-                $adminView->editBill();
-            } 
-            else 
-            {
-                throw new Exception('Veuillez ne jamais modifier manuellement l\'URL de votre navigateur. Faites "précédent" pour ne pas perdre votre travail.');
-            }
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-        }
-    
-        elseif($_GET['action'] === 'editionValidation')
-        {
-=======
             $adminView->deconnexion();
         }
     
@@ -275,7 +110,6 @@ if (isset($_GET['action'])) {
     
         elseif($_GET['action'] === 'editionValidation')
         {
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
             if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
             {
                 $adminView->editionValidation($_GET['id']);
@@ -288,12 +122,6 @@ if (isset($_GET['action'])) {
     
         elseif($_GET['action'] === 'deleteValidation')
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
-=======
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
             if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT) > 0) 
             {
                 $adminView->deleteValidation($_GET['id']);
@@ -302,10 +130,6 @@ if (isset($_GET['action'])) {
             {
                 throw new Exception('Veuillez ne jamais modifier manuellement l\'URL de votre navigateur. Faites "précédent" pour ne pas perdre votre travail.');
             }
-<<<<<<< HEAD
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
         }
     
         elseif($_GET['action'] === 'editComment')
@@ -322,12 +146,6 @@ if (isset($_GET['action'])) {
     
         elseif($_GET['action'] === 'deleteCommentValidation')
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
-=======
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
             if (isset($_GET['commentId']) && filter_var($_GET['commentId'], FILTER_VALIDATE_INT) > 0) 
             {
                 $adminView->deleteCommentValidation($_GET['commentId']);
@@ -336,10 +154,6 @@ if (isset($_GET['action'])) {
             {
                 throw new Exception('Veuillez ne jamais modifier manuellement l\'URL de votre navigateur. Faites "précédent" pour ne pas perdre votre travail.');
             }
-<<<<<<< HEAD
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
         }
     
         elseif($_GET['action'] === 'report')
@@ -356,15 +170,7 @@ if (isset($_GET['action'])) {
     
         elseif($_GET['action'] === 'legal')
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
             $userView->legal();
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
-            $userView->legal();
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
         }
     
     
@@ -372,47 +178,29 @@ if (isset($_GET['action'])) {
         {
             $adminView->allReportedComments();
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        else 
-        {
-            header('Location:https://www.alpha-gecko.com/Projet_4/index.php?action=error');
-=======
     
     
         else {
             $userView->error();
->>>>>>> 5713764... Modification de la logique de redirection, car non-applicable sur un serveur qui n'est pas local.
-=======
-    
-        else {
-            $userView->error();
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
         }
         
     }
-<<<<<<< HEAD
 
     catch (Exception $e)
     {
         var_dump($e->getMessage());
     }
-=======
-    catch (Exception $e) // On va attraper les exceptions "Exception" s'il y en a une qui est levée
-    {
-        echo 'Une exception a été lancée. Message d\'erreur : ' . $e->getMessage();
-    }   
->>>>>>> e80ed29... Modification de l'adminController afin de passer les attributs de public à privé, création des getters correspondant.
 }
+
 
 /* default views */ 
 
-elseif (isset($_SESSION['admin']) && !isset($_GET['action']))
+else if (isset($_SESSION['admin']))
 {
     $adminView->adminPanel();
 }
 
-elseif (!isset($_SESSION['admin']) && !isset($_GET['action'])) 
+else 
 {
     $userView->billsList();
 }
